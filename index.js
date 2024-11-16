a54090b7eda6ff3d6c1da10035d1fa31720e42e9
Nathan G <73437724+ndg8743@users.noreply.github.com>
2024-11-15 19:48:13 -0500

    working ish

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 app.use(express.json());
 
@@ -8,20 +9,28 @@ const projectRouter = require('./server/routes/project');
 const commentRouter = require('./server/routes/comment');
 
 // CORS Middleware
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-// Route Handling
+// API Route Handling
 app.use('/user', userRouter);
 app.use('/project', projectRouter);
 app.use('/comment', commentRouter);
 
+// Serve Static Frontend Files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Default Route to Serve HTML Files
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/html/index.html'));
+});
+
 // Server Setup
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}/`);
 });
